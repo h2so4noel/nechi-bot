0cdf4b860d4dbe2dffee5e2a66ddafe7548679a2
Pawin S <pawin.s@taskworld.com>
2017-08-01 16:59:49 +0700

    added lodash and functions for bot

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
+const _ = require('lodash')
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
-console.log(cryObject)
 
 const umuToken = 'MzQxODY0MTM0MzQ3OTgwODAx.DGHXoQ.exsnNhKELtR4O9a-NEj-jusjiYA'
 const nechiCreds = [
   'nechi',
   'เนชิ',
-  'Nechi'
+  'Nechi',
+  'Nechigawara',
+  '@Nechigawara'
+]
+const cryCreds = [
+  'แง้',
+  'QAQ',
+  'cry',
+  'cri'
 ]
 
+function getEmbeds () {
+  const embeds = []
+  const cry = new Discord.RichEmbed()
+    .setTitle('BibleThump')
+    .attachFile('images/cry.png')
+  const umu = new Discord.RichEmbed()
+    .setTitle('Umu')
+    .attachFile('images/umu.png')
+
+  embeds.push(cry, umu)
+
+  return embeds
+}
+
 client.on('ready', () => {
-  console.log('I am ready!');
+  console.log('Umu, ready for action!');
 });
 
 client.on('message', message => {
-  const cryObject = new Discord.RichEmbed()
-    .setTitle('cry.png')
-    .attachFile('images/cry.png')
+  const embedsObj = getEmbeds()
+  const embeds = (embedTitle) => {
+    return _.find(embedsObj, (obj) => {
+      return obj.title === embedTitle
+    })
+  }
+
   if (nechiCreds.includes(message.content)) {
-    message.reply('Testing embed', {embed: cryObject});
+    message.reply('อุมุ', {embed: embeds('Umu')});
+  }
+  else if (cryCreds.includes(message.content)) {
+    message.reply('แง้', {embed: embeds('BibleThump')});
   }
 });
 
